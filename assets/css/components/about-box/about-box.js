e0c27050ec56f5467d58f5a77a82ff893125e29e
Wesley Alves <wesley.alves@cd.com>
2018-04-06 19:00:43 -0300

    - Bug pulando um square na navegação da about box; - Caption em tooltip nas serviços

--- assets/css/components/about-box/about-box.js
+++ assets/css/components/about-box/about-box.js
@@ -3,47 +3,56 @@ var i = 0,
     total = content.outerHeight();
 
 // Click Scrolling
+
+($(".about-box-content>p").outerHeight() < $(".about-box-content").outerHeight()) ? $(".text-down").addClass("-disabled") : '';
+
+$(window).resize(function() {
+    ($(".about-box-content>p").outerHeight() < $(".about-box-content").outerHeight()) ? $(".text-down").addClass("-disabled") : $(".text-down").removeClass("-disabled");
+});    
     
 function textScrolling(e){
     var e = $(e);
-    if(e.attr("class") == "text-up"){
-        (i < 0) ? i++ : '';
-        (i*40 > -total - -150) ? $(".text-down").removeClass("-disabled") : '';
-        (i >= 0 && !$(".text-up").is(".-disabled")) ? $(".text-up").addClass("-disabled") : '';
-        (i*40 > -total*0.99) ? $(".about-box-pagination li:nth-child(4)").removeClass("-active") : '';   
-        (i*40 > -total*0.66) ? $(".about-box-pagination li:nth-child(3)").removeClass("-active") : '';
-        (i*40 > -total*0.33) ? $(".about-box-pagination li:nth-child(2)").removeClass("-active") : '';       
-    } else {
-        (i*40 > -total - -150) ? i-- : '';
-        (i*40 < -total - -150) ? $(".text-down").addClass("-disabled") : '';
-        (i < 0 && $(".text-up").is(".-disabled")) ? $(".text-up").removeClass("-disabled") : '';
-        (i*40 < -total*0.99 + 150) ? $(".about-box-pagination li:nth-child(4)").addClass("-active") : '';   
-        (i*40 < -total*0.66) ? $(".about-box-pagination li:nth-child(3)").addClass("-active") : '';
-        (i*40 < -total*0.33) ? $(".about-box-pagination li:nth-child(2)").addClass("-active") : '';       
-    }    
-    content.css("top", i*40);
+    if($(".about-box-content>p").outerHeight() > $(".about-box-content").outerHeight()){
+        if(e.attr("class") == "text-up"){
+            (i < 0) ? i++ : '';
+            (i*40 > -total - -150) ? $(".text-down").removeClass("-disabled") : '';
+            (i >= 0 && !$(".text-up").is(".-disabled")) ? $(".text-up").addClass("-disabled") : '';
+            (i*40 > -total*0.99) ? $(".about-box-pagination li:nth-child(4)").removeClass("-active") : '';   
+            (i*40 > -total*0.66) ? $(".about-box-pagination li:nth-child(3)").removeClass("-active") : '';
+            (i*40 > -total*0.33) ? $(".about-box-pagination li:nth-child(2)").removeClass("-active") : '';       
+        } else {
+            (i*40 > -total - -150) ? i-- : '';
+            (i*40 < -total - -150) ? $(".text-down").addClass("-disabled") : '';
+            (i < 0 && $(".text-up").is(".-disabled")) ? $(".text-up").removeClass("-disabled") : '';
+            (i*40 < -total*0.99 + 150) ? $(".about-box-pagination li:nth-child(4)").addClass("-active") : '';   
+            (i*40 < -total*0.66) ? $(".about-box-pagination li:nth-child(3)").addClass("-active") : '';
+            (i*40 < -total*0.33) ? $(".about-box-pagination li:nth-child(2)").addClass("-active") : '';       
+        }    
+        content.css("top", i*40);
+    }
 }
 
 // Mousewheel
 
 $(document).ready(function () {
     $(".about-box-content").bind("mousewheel", function(event, delta) {
-        console.log(delta);
-        (delta < 0) ? (i*40 > -total - -150) ? i-- : '' : (i < 0) ? i++ : '';
-        (i*40 > -total - -150) ? $(".text-down").removeClass("-disabled") : '';
-        (i >= 0 && !$(".text-up").is(".-disabled")) ? $(".text-up").addClass("-disabled") : '';
-        (i*40 < -total - -150) ? $(".text-down").addClass("-disabled") : '';
-        (i < 0 && $(".text-up").is(".-disabled")) ? $(".text-up").removeClass("-disabled") : '';
-
-        (i*40 > -total*0.99) ? $(".about-box-pagination li:nth-child(4)").removeClass("-active") : '';   
-        (i*40 > -total*0.66) ? $(".about-box-pagination li:nth-child(3)").removeClass("-active") : '';
-        (i*40 > -total*0.33) ? $(".about-box-pagination li:nth-child(2)").removeClass("-active") : '';  
-
-        (i*40 < -total*0.99 + 150) ? $(".about-box-pagination li:nth-child(4)").addClass("-active") : '';   
-        (i*40 < -total*0.66) ? $(".about-box-pagination li:nth-child(3)").addClass("-active") : '';
-        (i*40 < -total*0.33) ? $(".about-box-pagination li:nth-child(2)").addClass("-active") : '';   
-
-        $(this).find("p").css("top", i*40);
+        if($(this).find("p").outerHeight() > $(this).outerHeight()){
+            (delta < 0) ? (i*40 > -total - -150) ? i-- : '' : (i < 0) ? i++ : '';
+            (i*40 > -total - -150) ? $(".text-down").removeClass("-disabled") : '';
+            (i >= 0 && !$(".text-up").is(".-disabled")) ? $(".text-up").addClass("-disabled") : '';
+            (i*40 < -total - -150) ? $(".text-down").addClass("-disabled") : '';
+            (i < 0 && $(".text-up").is(".-disabled")) ? $(".text-up").removeClass("-disabled") : '';
+    
+            (i*40 > -total*0.99) ? $(".about-box-pagination li:nth-child(4)").removeClass("-active") : '';   
+            (i*40 > -total*0.67) ? $(".about-box-pagination li:nth-child(3)").removeClass("-active") : '';
+            (i*40 > -total*0.33) ? $(".about-box-pagination li:nth-child(2)").removeClass("-active") : '';  
+    
+            (i*40 < -total*0.99 + 150) ? $(".about-box-pagination li:nth-child(4)").addClass("-active") : '';   
+            (i*40 < -total*0.66) ? $(".about-box-pagination li:nth-child(3)").addClass("-active") : '';
+            (i*40 < -total*0.33) ? $(".about-box-pagination li:nth-child(2)").addClass("-active") : '';   
+    
+            $(this).find("p").css("top", i*40);
+        }
     });
     $(".about-box").hover(function() {
         $("body").css("overflow","hidden")
