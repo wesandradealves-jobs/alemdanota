29bce0f8d21204953cd964cb720defbbf19f0529
Wesley Alves <wesley.alves@cd.com>
2018-04-19 19:58:35 -0300

    - Correção do form de email

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,16 +8,16 @@ function copyright() {
     $(".main")
     .append("<a class='copyright' target='_blank' title='Concebido por Innominatum Consultoria' href='http://innominatum.eu/'><img alt='Concebido por Innominatum Consultoria' src='assets/imgs/svg/copyright.svg' height='35'/> <span class='copyright-text'>Concebido por Innominatum Consultoria</span></a>")
 }
-function whatsapp(){
+function sendWPP(e){
+    var e = $(e);
     var telefone = "5521973819591";
     var saudacao = "Olá! Meu nome é";
-    var whatsappbutton = document.getElementById("whatsapp");
     var name = document.getElementById("user_nome").value;
     var msg = document.getElementById("user_msg").value;		
     var saudacaoencode = encodeURI(saudacao);		
     var url_base = "https://api.whatsapp.com/send?phone=" + telefone + "&text=" + saudacaoencode + "%20" + encodeURI(name) + "%20e%20" + encodeURI(msg);
 
-    whatsappbutton.setAttribute("formaction", url_base);	
+    e.attr("href", url_base);	
 }
 function spinner(){
     $(".book").append("<div class='loading-screen'><div class='loader'/></div></div>");
